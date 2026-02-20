8aa7a8609eb825369b9e119b9b769cbd68bb8cb9
fatima BAALI <132785369+Fatma23233@users.noreply.github.com>
2026-02-20 17:54:37 +0100

    Change app.run to use environment variables

--- TP/EX1.py
+++ TP/EX1.py
@@ -118,7 +118,10 @@ def delete_student(id):
     students = [s for s in students if s['id'] != id]
     return jsonify({"message": f"Étudiant {id} supprimé"}), 200
     
+#if __name__ == '__main__':
+    #app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 
